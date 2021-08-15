7724f4776ba22731f322b020723f38a7d3958278
JSONRete <jsonrete@gmail.com>
2021-08-15 05:13:54 -0400

    commit tree fc80291dfd951ad9dc1f06d95e68b5ee9969ab0d

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+
+
+
+
+
+function map(src, callback) {
+    const r = [];
+  
+    for (let i = 0; i < src.length; i++) {
+      const theElement = src[i];
+      r.push(callback(theElement));
+    }
+  
+    return r;
+  }
+  
